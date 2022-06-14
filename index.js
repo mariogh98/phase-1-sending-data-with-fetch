9923a32fe42d6b53cd96abf1538d24c3df4038c6
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-06-14 00:28:12 -0500

    commit tree 9cb63ad1afa583d4bf82c80e770e3a400d232c20

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email:`${email}`
+        })
+
+    })
+
+    .then(response => response.json())
+    .then(newObject => document.body.innerHTML = newObject.id)
+
+
+
+    .catch(error => document.body.innerHTML = error.message)
+
+}
